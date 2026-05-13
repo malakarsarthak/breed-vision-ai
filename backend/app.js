5da5c61fd3f6ba1e5b8558b8c4c431e73f6954b1
malakarsarthak <sarthakmalakar2004@gmail.com>
2026-05-13 18:46:37 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,12 @@ app.get('/health', (req, res) => {
 });
 
 // const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.listen(3001, '0.0.0.0', () => {
-    console.log("Server running on 127.0.0.1:3001");
+//  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(3001, '0.0.0.0', () => {
+//     console.log("Server running on 127.0.0.1:3001");
+// });
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
